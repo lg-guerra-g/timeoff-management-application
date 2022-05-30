0812fdfadfe08fc018bc18e39f1de1672fdf6826
Luis Gerardo Guerra Garrido <71341599+lg-guerra-g@users.noreply.github.com>
2022-05-30 01:21:37 -0500

    Add documentation details, correct cloudfront solution (#2)

--- infra/data.tf
+++ infra/data.tf
@@ -1,7 +1,3 @@
-data "aws_cloudfront_cache_policy" "optimized" {
-  name = "Managed-CachingOptimized"
-}
-
 data "aws_iam_policy" "beanstalk_updates" {
   arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy"
 }
